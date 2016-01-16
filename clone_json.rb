0d1e2efe0a523125ea2d1f391b81e96ca6e25bf1
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-16 07:42:24 +0000

    Uses fixed config

--- clone_json.rb
+++ clone_json.rb
@@ -60,7 +60,7 @@ end
 
 # Main
 
-file = File.new('repos.json', 'r')
+file = File.new("#{ENV['HOME']}/.setup_environment/repos.json", 'r')
 file_config = JSON.parse(file.read)
 
 process_bundle(file_config)
