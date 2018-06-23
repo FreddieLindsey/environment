e1088134c532945c33597fa3ac791fd14e52f699
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2018-06-23 22:48:02 +0100

    Install zprezto

---
+++ install_zprezto.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
+
+rsync -avz ./files/zprezto/ ~/.zprezto/
--- setup.sh
+++ setup.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
 notify () {
-  echo -e "-------------------------------------"
+  echo -e "--------------------------------------------------------------------------"
   echo -e "--- $1"
-  echo -e "-------------------------------------"
+  echo -e "--------------------------------------------------------------------------"
+}
+
+notify-start () {
+  notify "INSTALLING $1"
+}
+
+notify-end () {
+  notify "INSTALLED $1"
+  echo -e "\n"
 }
 
 # Install homebrew
-notify "INSTALLING HOMEBREW"
+notify-start "HOMEBREW"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-notify "INSTALLED HOMEBREW"
+notify-end "HOMEBREW"
 
 # Install normal brews
-notify "INSTALLING MAC APP STORE CLI"
-brew install mas
-notify "INSTALLED MAC APP STORE CLI"
+BREWS=(mas pyenv pyenv-virtualenv rbenv nvm)
+for i in "${BREWS[@]}"; do
+  notify-start "$(echo $i | awk '{print toupper($0)}')"
+  brew install "$i"
+  notify-end "$(echo $i | awk '{print toupper($0)}')"
+done
 
 # Install cask brews
-notify "INSTALLING ITERM2, VISUAL STUDIO CODE, GOOGLE CHROME, SPOTIFY"
-brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install google-chrome
-brew cask install spotify
-notify "INSTALLED ITERM2, VISUAL STUDIO CODE, GOOGLE CHROME, SPOTIFY"
+CASK_BREWS=(bartender istat-menus iterm2 visual-studio-code google-chrome spotify vlc)
+for i in "${CASK_BREWS[@]}"; do
+  notify-start "$(echo CASK\ $i | awk '{print toupper($0)}')"
+  brew cask install "$i"
+  notify-end "$(echo CASK\ $i | awk '{print toupper($0)}')"
+done
 
 # Install apps from Mac App Store
-notify "INSTALLING MOOM, SCANSNAP CLOUD, SCREENS 3, SLACK, WUNDERLIST"
-mas install "$(mas search Moom | head -n 1 | awk -F ' ' '{print $1}')"
-mas install "$(mas search ScanSnap\ Cloud | head -n 1 | awk -F ' ' '{print $1}')"
-mas install 446107677 # Screens 3
-mas install "$(mas search Slack | head -n 1 | awk -F ' ' '{print $1}')"
-mas install "$(mas search Wunderlist | head -n 1 | awk -F ' ' '{print $1}')"
-notify "INSTALLED MOOM, SCANSNAP CLOUD, SCREENS 3, SLACK, WUNDERLIST"
+MAS_APPS_AUTO=(Moom ScanSnap\ Cloud Slack Spark WhatsApp\ Desktop Wunderlist)
+for i in "${MAS_APPS_AUTO[@]}"; do
+  notify-start "$(echo $i | awk '{print toupper($0)}')"
+  mas install "$(mas search "$i" | head -n 1 | awk -F ' ' '{print $1}')"
+  notify-end "$(echo $i | awk '{print toupper($0)}')"
+done
+
+MAS_APPS_MANUAL=(446107677)
+for i in "${MAS_APPS_MANUAL[@]}"; do
+  notify-start "$(echo $i | awk '{print toupper($0)}')"
+  mas install "$i"
+  notify-end "$(echo $i | awk '{print toupper($0)}')"
+done
+
+# Install zprezto
+notify-start "ZPREZTO"
+./install_zprezto.sh
+notify-end "ZPREZTO"
