bd83e33679e4d6d133ea7be0996b2488367584d9
Freddie Lindsey <freddie@Mac-Mini.local>
2018-06-30 15:49:52 +0100

    Add vscode settings

--- install_extensions.sh
+++ install_extensions.sh
@@ -18,7 +18,11 @@ notify-end () {
   echo -e "\n"
 }
 
-VSCODE_EXTENSIONS=("james-yu.latex-workshop")
+notify-start "VSCODE CONFIG"
+cp files/vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
+notify-end "VSCODE CONFIG"
+
+VSCODE_EXTENSIONS=("james-yu.latex-workshop" "timonwong.shellcheck")
 for i in "${VSCODE_EXTENSIONS[@]}"; do
   notify-start "$(echo "VSCODE EXTENSION $i" | awk '{print toupper($0)}')"
   code --install-extension "$i"
